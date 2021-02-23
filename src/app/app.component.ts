f67f5c0ff8bba1d5936ec96fa05e5664decb595c
Trigalo27 <internick27@gmail.com>
2021-02-23 10:52:49 -0600

    commit a material demo 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo1';
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  minDate = new Date();
+  maxDate = new Date(1981, 6, 5);
 }
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox'; 
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select' ; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatDatepickerModule, MatNativeDateModule} from '@angular/material'; 
 
 @NgModule({
   declarations: [
@@ -10,9 +15,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+ }
